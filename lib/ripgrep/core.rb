2ccd35c8c30cce7363bf198136fa1ed12178f481
motoki-shun <motoki-shun@kayac.com>
2019-05-05 16:24:54 +0900

    specify dir. and fix opts/args

--- lib/ripgrep/core.rb
+++ lib/ripgrep/core.rb
@@ -2,8 +2,14 @@ require 'open3'
 
 module Ripgrep
   class Core
-    def self.exec(*opts)
-      stdout, stderr, status = Open3.capture3('rg', *opts)
+    def self.exec(*args, opts)
+      unless opts.class == Hash
+        args << opts
+        opts = {}
+      end
+      opts = { dir: '.' }.merge(opts)
+      puts "args: #{args}, opts: #{opts}"
+      stdout, stderr, status = Open3.capture3('rg', *args, opts[:dir])
       unless [0, 1].include? status.exitstatus
         puts "exit status: #{status.exitstatus}"
         raise Ripgrep::CommandExecutionError, stderr 
