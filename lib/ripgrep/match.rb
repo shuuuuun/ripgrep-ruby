c8070f4eb43daf6103cf574ef6670ab86d2aebdb
motoki-shun <motoki-shun@kayac.com>
2019-07-27 13:31:15 +0900

    Match#to_s

--- lib/ripgrep/match.rb
+++ lib/ripgrep/match.rb
@@ -6,6 +6,10 @@ module Ripgrep
       @file = file
       @body = body
     end
+
+    def to_s
+      { file: file, body: body }.to_json
+    end
     # TODO: make more rich and useful interface.
   end
 end
